8b6889b5ef3567a03d711b057f4149d7b788723d
csalguera <carlos.e.salguera@gmail.com>
2025-02-19 17:33:51 -0500

    add functionality to functions.js file

--- week-6/js/functions.js
+++ week-6/js/functions.js
@@ -1,1 +1,28 @@
+const items = [18, 24, 17, 3]
 
+function calculateTotal(price, tax = 0.07, discountRate = 0.1) {
+  if (price > 100) {
+    return price * (1 + tax) * (1 - discountRate * 2)
+  } else if (price > 50) {
+    return price * (1 + tax) * (1 - discountRate)
+  } else {
+    return price * (1 + tax)
+  }
+}
+
+function showItems() {
+  const newSection = document.createElement('section')
+  newSection.innerHTML = '<h4>Items</h4>'
+
+  let total = 0
+  for (let i = 0; i < items.length; i++) {
+    newSection.innerHTML += `<p>$${items[i]}</p>`
+    total += items[i]
+  }
+
+  newSection.innerHTML += '<hr />'
+  newSection.innerHTML += `<p>$${calculateTotal(total)}</p>`
+  document.body.appendChild(newSection)
+}
+
+showItems()
