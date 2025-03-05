654c114ba171f87850eaa450567eede9dabdd691
csalguera <carlos.e.salguera@gmail.com>
2025-03-05 16:38:11 -0500

    update window.html and add functionality to window.js

---
+++ week-8/js/window.js
@@ -0,0 +1,11 @@
+console.log('Window 7 is still the only OS to use');
+
+// alert('Run away now!!!')
+
+const getFavoritePeepFlavor = () => {
+  const flavor = prompt("What's your favorite peep flavor?", "Regular")
+  console.log("Favorite flavor:", flavor);
+}
+
+const chooseToLeave = confirm("Are you going to go get the flyers?")
+console.log('Ty is getting the flyers:', chooseToLeave);
